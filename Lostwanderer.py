b6513210dbba35d054c6ffba1a91ce18597f8251
Jierui Shi <js2515@cam.ac.uk>
2020-04-25 17:00:22 +0800

    Made start page main into single function Made stepCount attribute of wanderer Add cleardata method to wanderer for return purposes in mainpage

--- Lostwanderer.py
+++ Lostwanderer.py
@@ -27,6 +27,12 @@ class wanderer:
         self.xstart = x
         self.ystart = y
         self.canvas = canvas
+        # record furtherst distance from centre, used for radius of circle
+        self.distance_max = 0
+        # record step count since start of run
+        self.stepCount = 0
+        # record all the positions, draw them in every frame
+        self.positionSet = set()
 
     def moveright(self):
         self.x += self.vel
@@ -52,21 +58,30 @@ class wanderer:
     def distance_from_start(self):
         return math.sqrt((self.x - self.ystart)**2 + (self.y - self.ystart)**2)
 
-    # record furtherst distance from centre, used for radius of circle
-    distance_max = 0
-    # record all the positions, draw them in every frame
-    positionSet = set()
-
-    def update_position_and_distance(self):
+    def update_position_and_distance_and_stepCount(self):
+        # Add more position to position set
         self.positionSet.add((self.x,self.y))
+        #update max distance
         if self.distance_from_start() > self.distance_max:
             self.distance_max = self.distance_from_start()
+        self.stepCount += 1
 
     def display(self):
         """shows all the position that the lost wanderer has been to and the furthest distance"""
         for n in self.positionSet:
             pygame.draw.rect(self.canvas, (255, 255, 255), (n[0], n[1], 1, 1))
         pygame.draw.circle(self.canvas, (0, 255, 0), (int(self.xstart), int(self.ystart)), int(self.distance_max), 1)
+        #arbitrary rectangle to mark the beginning point
+        pygame.draw.rect(screen, (255, 0, 0), (Jerry.xstart - 2, Jerry.ystart - 2, 5, 5))
+
+    def clearData(self):
+        """clear all data in the current instance, (for new run)"""
+        self.positionSet = set()
+        self.distance_max = 0
+        self.x = self.xstart
+        self.y = self.ystart
+        self.stepCount = 0
+
 
 Jerry = wanderer(screenSize/2,screenSize/2,2)
 
@@ -76,20 +91,17 @@ def showWord(text,position,colour = WHITE, size = 20):
     screen.blit(myText,position)
 
 #runPage
-stepCount = 0
 def stepChange():
     Jerry.randommove()
-    Jerry.update_position_and_distance()
-    global stepCount
-    stepCount += 1
+    Jerry.update_position_and_distance_and_stepCount()
+
 def update():
     screen.fill((0,0,0))
     Jerry.display()
-    pygame.draw.rect(screen,(255,0,0),(Jerry.xstart - 2,Jerry.ystart - 2,5,5))
     showWord("A lost wanderer is moving two pixels per step.", (20, 50), size = 18)
     showWord("The green circle indicates the furthest distance", (20, 70), size = 18)
     showWord("from the initial position.", (20, 90), size = 18)
-    showWord("Steps:" + "    " + str(stepCount), (screenSize - 200, 50))
+    showWord("Steps:" + "    " + str(Jerry.stepCount), (screenSize - 200, 50))
     showWord("Max Distance:" + str(round(Jerry.distance_max,2)), (screenSize - 200, 80))
     button1.show()
     pygame.display.update()
@@ -98,23 +110,14 @@ button1 = Button(screen,screenSize - 100, screenSize - 50, 60, 30, WHITE, GREY, 
 runButton = [button1]
 
 #startPage
-def startPage():
+def startUpdate():
     screen.fill((0,0,0))
     for button in startButton:
         button.show()
     pygame.display.update()
-    if button2.leftClicked():
-        global runPage
-        global startPage
-        runPage = True
-        startPage = False
-    if button3.leftClicked():
-        pass
-    if button4.leftClicked():
-        global run
-        run = False
+
 button2 = Button(screen,screenSize/2, screenSize/2 - 50, 150, 40, WHITE, GREY, BLUE, textcolour = BLACK, textcolour2 = BLACK, TEXT = "Start")
-button3 = Button(screen,screenSize/2, screenSize/2, 150, 40, WHITE, GREY, BLUE, textcolour = BLACK, textcolour2 = BLACK, TEXT = "Instructions")
+button3 = Button(screen,screenSize/2, screenSize/2, 150, 40, WHITE, GREY, BLUE, textcolour = BLACK, textcolour2 = BLACK, TEXT = "Settings")
 button4 = Button(screen,screenSize/2, screenSize/2 + 50, 150, 40, WHITE, GREY, BLUE, textcolour = BLACK, textcolour2 = BLACK, TEXT = "Quit")
 startButton = [button2,button3,button4]
 
@@ -135,13 +138,11 @@ while run:
         if button1.leftClicked():
             runPage = False
             startPage = True
+            Jerry.clearData()
 
     elif startPage:
         clock.tick(50)
-        screen.fill((0, 0, 0))
-        for button in startButton:
-            button.show()
-        pygame.display.update()
+        startUpdate()
         if button2.leftClicked():
             runPage = True
             startPage = False
